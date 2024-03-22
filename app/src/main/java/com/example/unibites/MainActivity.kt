51b1d3c55468e75a3e576269376669cf373f337e
dfcruza <60239198+dfcruza@users.noreply.github.com>
2024-03-22 17:45:16 -0500

    Definicion arquitectura mvm

--- app/src/main/java/com/example/unibites/MainActivity.kt
+++ app/src/main/java/com/example/unibites/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.unibites
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.unibites.ui.theme.UniBitesTheme
 
@@ -20,9 +25,10 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.onSecondary
                 ) {
-                    Greeting("iOS")
+                        Greeting("Android app")
+
                 }
             }
         }
